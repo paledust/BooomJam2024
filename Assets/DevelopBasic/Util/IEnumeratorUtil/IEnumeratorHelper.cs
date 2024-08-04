26bec1cf87ddf5750f95f386b75d21f0812ccdf8
paledust <paledust@hotmail.com>
2024-08-04 18:22:32 +0800

    update playercontrol

--- Assets/DevelopBasic/Util/IEnumeratorUtil/IEnumeratorHelper.cs
+++ Assets/DevelopBasic/Util/IEnumeratorUtil/IEnumeratorHelper.cs
@@ -69,6 +69,9 @@ public static class CommonCoroutine{
         yield return new WaitForSeconds(delay);
         GameObject.Destroy(gameObject);
     }
+    public static IEnumerator coroutineLoop(float duration, Action<float> action){
+        yield return new WaitForLoop(duration, (t)=>action(t));
+    }
 }
 
 public class CoroutineExcuter
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,31 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class PlayerControl : MonoBehaviour
 {
-    public MouseLook m_mouseLook;
-
-    private Vector3 hoverPos;
+    [SerializeField] private MouseLook m_mouseLook;
+#region PP
+    [SerializeField] private float focusTime = 0.2f;
+    [SerializeField] private Volume focusVolume; 
+#endregion
     private Camera mainCam;
 
+    public Vector3 m_hoverPos{get; private set;}
     public Basic_Clickable m_hoveringInteractable{get; private set;} //The hovering interactable
     public Basic_Clickable m_holdingInteractable{get; private set;} //Currently holding interactable (Not hold by Maie's hand, but by cursor)
 
+    private CoroutineExcuter ppFader;
     void Start(){
         mainCam = Camera.main;
         m_mouseLook.Init();
+        ppFader = new CoroutineExcuter(this);
     }
     void Update(){
         m_mouseLook.UpdateLookRotation();
         Ray ray = mainCam.ViewportPointToRay(Vector2.one*0.5f);
         if(Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, Service.interactableLayerMask)){
             Basic_Clickable hit_Interactable = hit.collider.GetComponent<Basic_Clickable>();
-            hoverPos = hit.point;
+            m_hoverPos = hit.point;
             if(hit_Interactable!=null){
                 if(m_hoveringInteractable != hit_Interactable) {
                     if(m_hoveringInteractable!=null) m_hoveringInteractable.OnExitHover();
-
+                    ppFader.Excute(CommonCoroutine.coroutineLoop(focusTime, (t)=>focusVolume.weight = Mathf.Lerp(0,1,EasingFunc.Easing.QuadEaseOut(t))));
                     m_hoveringInteractable = hit_Interactable;
                     if(m_hoveringInteractable.IsAvailable) m_hoveringInteractable.OnHover(this);
                 }
@@ -43,6 +49,7 @@ public class PlayerControl : MonoBehaviour
         if(m_hoveringInteractable != null){
             m_hoveringInteractable.OnExitHover();
             m_hoveringInteractable = null;
+            ppFader.Excute(CommonCoroutine.coroutineLoop(focusTime, (t)=>focusVolume.weight = Mathf.Lerp(1,0,EasingFunc.Easing.QuadEaseOut(t))));
         }
     }
     public void HoldInteractable(Basic_Clickable interactable){
@@ -77,6 +84,9 @@ public class PlayerControl : MonoBehaviour
         Vector2 input = value.Get<Vector2>();
         m_mouseLook.LookRotation(input);
     }
+#endregion
+
+#region Coroutine
 
 #endregion
 }
